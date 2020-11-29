7fd0bfabbaf2ffe623a2df73ade79ff9e95a9d78
sjhello <sjhello235@gmail.com>
2020-11-29 14:01:15 +0900

    add chapter2

---
+++ src/com/sjhello/chapter2/Profile.java
@@ -0,0 +1,14 @@
+package com.sjhello.chapter2;
+
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("My name is sjhello");
+
+        Profile profile = new Profile();
+        profile.printAge();
+    }
+
+    public void printAge() {
+        System.out.println("My Age is 29");
+    }
+}
