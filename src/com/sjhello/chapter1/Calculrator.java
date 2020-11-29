f38e336031f2c2cb008d7584bb4a6c70915040ad
sjhello <sjhello235@gmail.com>
2020-11-29 13:55:10 +0900

    add chapter1

---
+++ src/com/sjhello/chapter1/Calculrator.java
@@ -0,0 +1,25 @@
+package com.sjhello.chapter1;
+
+public class Calculrator {
+    int result;
+
+    public static void main(String[] args) {
+    }
+
+    public int substract(int num1, int num2) {
+        result = num1- num2;
+        return result;
+    }
+
+    public int multipy(int num1, int num2) {
+        result = num1 * num2;
+        return result;
+    }
+
+    public int divide(int num1, int num2) {
+        result = num1 / num2;
+        return result;
+    }
+}
+
+
